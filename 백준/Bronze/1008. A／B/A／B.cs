9268118181c9dae44794c446736880c1e6209035
shin-su <62335496+shin-su@users.noreply.github.com>
2023-03-05 16:37:48 +0900

    [Bronze V] Title: A/B, Time: 64 ms, Memory: 5492 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            Console.WriteLine(double.Parse(s[0]) / double.Parse(s[1]));
+        }
+    }
+}
