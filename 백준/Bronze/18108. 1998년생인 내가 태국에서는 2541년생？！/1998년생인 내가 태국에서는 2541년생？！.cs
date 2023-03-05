df9d3cab98af501081730dc5fb419ab64bc43141
shin-su <62335496+shin-su@users.noreply.github.com>
2023-03-05 23:58:41 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 64 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Baekjoon
+{
+    class Algorithm
+    {
+        static void Main(string[] args)
+        {
+            string val = Console.ReadLine();
+            Console.WriteLine((int.Parse(val)) - 543);
+        }
+    }
+}
