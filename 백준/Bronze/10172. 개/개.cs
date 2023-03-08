0961ca97a85b8a815f337ed886fe6f45ab46c7dd
shin-su <62335496+shin-su@users.noreply.github.com>
2023-03-08 20:16:54 +0900

    [Bronze V] Title: 개, Time: 56 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cs
@@ -0,0 +1,17 @@
+using System;
+using static System.Console;
+
+namespace Baekjoon
+{
+    class Algorithm
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("|\\_/|");
+            WriteLine("|q p|   /}");
+            WriteLine("( 0 )\"\"\"\\");
+            WriteLine("|\"^\"`    |");
+            WriteLine("||_/=\\\\__|");
+        }
+    }
+}
