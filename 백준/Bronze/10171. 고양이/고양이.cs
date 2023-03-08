86fa4cbf2e37232982db4b58a9b45f050be2375d
shin-su <62335496+shin-su@users.noreply.github.com>
2023-03-08 20:14:24 +0900

    [Bronze V] Title: 고양이, Time: 64 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,16 @@
+using System;
+using static System.Console;
+
+namespace Baekjoon
+{
+    class Algorithm
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("\\    /\\");
+            WriteLine(" )  ( ')");
+            WriteLine("(  /  )");
+            WriteLine(" \\(__)|");
+        }
+    }
+}
